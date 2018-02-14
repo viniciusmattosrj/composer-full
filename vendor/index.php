1dfc973df169a5d1c7f6fc66439c13564018b4ec
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-02-14 18:28:49 -0200

    Projeto Update

---
+++ vendor/index.php
@@ -0,0 +1,22 @@
+<?php
+use \GUMP;
+
+require_once './vendor/autoload.php';
+
+$dadosForm = ['nome' => 'Vinícius Mattos', 'email' => 'vinimattos.rj@gmail.com'];
+
+$regras = [
+	'nome' => 'required|max_len,100|min_len,3',
+	'email' => 'required|max_len,150|valid_email'
+];
+
+$is_valid = GUMP::is_valid($dadosForm, $regras);
+
+if($is_valid === true) 
+{
+	echo 'Cadastrar os dados, correto!';
+} 
+else 
+{
+	print_r($is_valid);
+}
